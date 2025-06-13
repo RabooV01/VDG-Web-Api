349eb124ad7bdbfd9e3586b2df7610b85827930c
nowar aboamash <nowar901@gmail.com>
2025-06-13 08:55:37 +0300

    update IReservationRepostory

--- VDG_Backend/VDG_Web_Api/src/Repositories/Interfaces/IReservationRepository.cs
+++ VDG_Backend/VDG_Web_Api/src/Repositories/Interfaces/IReservationRepository.cs
@@ -2,11 +2,11 @@
 
 namespace VDG_Web_Api.src.Repositories.Interfaces
 {
-	public interface IReservationRepository
-	{
-		public Task BookAppointment(User user, VirtualClinic clinic, DateTime date);
-		public Task BookRevisionAppointment(User user, VirtualClinic clinic, DateTime date);
-		public Task CancelAppointment(int reservationId);
-		public Task<Reservation?> UpdateAppointment(Reservation reservation);
-	}
+    public interface IReservationRepository
+    {
+        public Task BookAppointment(User user, VirtualClinic clinic, DateTime date);
+        public Task BookRevisionAppointment(User user, VirtualClinic clinic, DateTime date);
+        public Task CancelAppointment(int reservationId);
+        public Task<Reservation?> UpdateAppointment(Reservation reservation);
+    }
 }
