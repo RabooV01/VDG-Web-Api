8093fbb3d211e21b8e7ef8e06fb39542d8963ba8
nowar aboamash <nowar901@gmail.com>
2025-06-20 16:01:14 +0300

    finish Repository Doctor

--- VDG_Backend/VDG_Web_Api/src/Repositories/Interfaces/IDoctorRepository.cs
+++ VDG_Backend/VDG_Web_Api/src/Repositories/Interfaces/IDoctorRepository.cs
@@ -4,11 +4,14 @@ namespace VDG_Web_Api.src.Repositories.Interfaces
 {
     public interface IDoctorRepository
     {
-        public Task<bool> AddDoctorAsync(int userId, string syndicateId, int specialityId);
+        public Task<int> AddDoctorAsync(Doctor doctor);
         public Task DeleteDoctorAsync(int doctorId);
         public Task UpdateDoctorAsync(Doctor doctor);
         public Task<Doctor?> GetDoctorByIdAsync(int id);
-        public Task<Doctor?> GetDoctorByNameAsync(string fullName);
+        public Task<Doctor?> GetDoctorBySyndicateIdAsync(string syndicateId);
+        public Task<IEnumerable<Doctor>?> GetDoctorsBySpecialityIdAsync(int specialityId);
+        public Task<IEnumerable<Doctor>?> GetDoctorsByNameAsync(string Name);
+        public Task<IEnumerable<Doctor>?> GetDoctorsByGenderAsync(string gender);
 
     }
 }
