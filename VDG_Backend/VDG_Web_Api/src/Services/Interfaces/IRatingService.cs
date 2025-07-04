6a76893a226519e59eaa69ffd2f42f3007688ad1
WORKSHOP <abd.alghafour2002@gmail.com>
2025-07-05 00:41:29 +0300

    Add Rating Service

---
+++ VDG_Backend/VDG_Web_Api/src/Services/Interfaces/IRatingService.cs
@@ -0,0 +1,9 @@
+﻿using VDG_Web_Api.src.DTOs.RatingDTOs;
+
+namespace VDG_Web_Api.src.Services.Interfaces
+{
+    public interface IRatingService
+    {
+        public Task Rate(RatingDTO ratingDto);
+    }
+}
