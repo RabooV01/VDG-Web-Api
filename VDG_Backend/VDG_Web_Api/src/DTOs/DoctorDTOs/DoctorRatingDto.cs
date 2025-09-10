b1276e7d9c8f12775f67bb1e7441a8a44892e802
nowar aboamash <nowar901@gmail.com>
2025-09-10 21:35:12 +0300

    update rating doctor

---
+++ VDG_Backend/VDG_Web_Api/src/DTOs/DoctorDTOs/DoctorRatingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VDG_Web_Api.src.DTOs.DoctorDTOs
+{
+    public class DoctorRatingDto
+    {
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public double Rating { get; set; }
+    }
+}
